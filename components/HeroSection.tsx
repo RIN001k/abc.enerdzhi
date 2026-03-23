26d6e52584f152f0c30a96348375b21bd38c2371
RIN001k <yurchenko050406@gmail.com>
2026-03-23 17:45:28 +0100

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -94,7 +94,7 @@ export function HeroSection() {
                   <p className="text-xs font-semibold text-slate-900">
                     Малі групи
                   </p>
-                  <p>до 10дітей</p>
+                  <p>до 10 дітей</p>
                 </div>
                 <div className="rounded-2xl bg-white px-3 py-2 shadow-sm">
                   <p className="text-xs font-semibold text-slate-900">
